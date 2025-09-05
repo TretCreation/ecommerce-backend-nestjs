352bd0cd5a76527be74a695a22beadd68e184e56
TretCreation <tret.creation@gmail.com>
2025-09-05 12:33:24 +0300

    chore: update eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { TaskModule } from "./task/task.module"
+import { ProductModule } from "./product/product.module"
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, ProductModule],
   controllers: [AppController],
   providers: [AppService]
 })
