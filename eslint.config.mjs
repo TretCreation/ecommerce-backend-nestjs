0e86fd0e2fc024d20a5d62d516c8e02d313cbe95
TretCreation <tret.creation@gmail.com>
2025-09-29 20:29:15 +0300

    eslint import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import importPlugin from "eslint-plugin-import"
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["eslint.config.mjs"]
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -15,20 +16,32 @@ export default tseslint.config(
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
+        ...globals.jest
       },
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        tsconfigRootDir: import.meta.dirname
+      }
     },
+    plugins: {
+      import: importPlugin
+    }
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true }
+        }
+      ]
+    }
+  }
+)
+
